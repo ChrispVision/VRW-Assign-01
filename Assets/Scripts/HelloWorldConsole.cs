0849d3b4e9131c4e8d338a3754f48e25f1498378
Chris_H <cd.hardwick@gmail.com>
2021-12-31 13:19:26 +0000

    Propose Console text to change to Hello Moon

--- Assets/Scripts/HelloWorldConsole.cs
+++ Assets/Scripts/HelloWorldConsole.cs
@@ -7,9 +7,9 @@ public class HelloWorldConsole : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //503403 Change console message to Hello Earth
+        //503403 Change console message to Hello Moon
         
-        Debug.Log("Hello Earth");
+        Debug.Log("Hello Moon");
     }
 
     // Update is called once per frame
