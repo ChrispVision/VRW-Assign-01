9fd81015e558db0f397e992aeea6bae5a3c5f410
Chris_H <cd.hardwick@gmail.com>
2021-12-31 13:14:26 +0000

    Add HelloWorldConsole to Alpha Branch

---
+++ Assets/Scripts/HelloWorldConsole.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorldConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //503403 Change console message to Hello Earth
+        
+        Debug.Log("Hello Earth");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
